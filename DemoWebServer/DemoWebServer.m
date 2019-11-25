9e32ba6c43b84928e1ec201e1925e8453d663646
Guy Salton <47520615+guysalton-codefresh@users.noreply.github.com>
2019-11-25 13:08:24 +0200

    Update DemoWebServer.m

--- DemoWebServer/DemoWebServer.m
+++ DemoWebServer/DemoWebServer.m
@@ -10,7 +10,7 @@ int main(int argc, const char* argv[]) {
                              requestClass:[GCDWebServerRequest class]
                              processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
       
-      return [GCDWebServerDataResponse responseWithHTML:@"<html><body><p>I love Codefresh</p></body></html>"];
+      return [GCDWebServerDataResponse responseWithHTML:@"<html><body><p>This is an OSX app hosted in the cloud</p></body></html>"];
       
     }];
     
